d7e1bad904a499e20be991eaf5b9dc69dac10367
sadaf walliyani <sadafwalliyani@gmail.com>
2025-03-01 16:31:19 +0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import os
 import streamlit as st
 
 # Define CSV file paths
-CSV_FILE_LIBRARY = 'library_data.csv'
+CSV_FILE_LIBRARY = 'aiswo-lms/library_data.csv'
 CSV_FILE_REGISTRATION = 'registration_newuser.csv'
 
 # Library Data Functions
